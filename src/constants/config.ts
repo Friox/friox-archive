3112c649a4c43edf78f111a044ff1a0106154647
Friox <devfriox@gmail.com>
2025-03-31 02:11:14 +0900

    first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  siteUrl: 'https://blog.friox.dev',
+  siteTitle: 'Friox Archive.',
+  siteDescription: '// Daily & Development',
+  userName: 'Friox',
+  userDescription: 'user description',
+  footerText: 'footer text',
+  footerCopyright: `© 2025. Friox All rights reserved.`,
+  githubLink: 'https://github.com/Friox',
+}
