94282deb2f7fb516da8efb0af229c8832a3396dc
Friox <devfriox@gmail.com>
2025-04-01 11:55:55 +0900

    fix: Resolve issue #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -65,7 +65,7 @@ export const pageQuery = graphql`
         frontmatter {
           slug
           category
-          createAt(formatString: "YYYY. MM. DD")
+          createAt(formatString: "YYYY. MM. DD.")
           title
           description
           tags
